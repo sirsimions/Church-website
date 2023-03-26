f9696cfea817ba4c570a97cbc58e52cb6c279a12
Simion <simion.okola@student.moringaschool.com>
2023-03-26 20:43:54 +0300

    debug

--- client/src/components/Sermon.js
+++ client/src/components/Sermon.js
@@ -4,20 +4,20 @@ import { useEffect, useState } from "react";
 
 function Sermon(){
 
-const [serm, setSerm] = useState('')
+// const [serm, setSerm] = useState('')
 
-useEffect(()=>{
-    fetch('/display')
-    .then(res=>res.json())
-    .then(data=>{
-        setSerm(data)
-        console.log(data)
-    })
-},[])
-    return(
-        <>
-        <div>Sermons</div>
-        </>
-    )
+// useEffect(()=>{
+//     fetch('/display')
+//     .then(res=>res.json())
+//     .then(data=>{
+//         setSerm(data)
+//         console.log(data)
+//     })
+// },[])
+//     return(
+//         <>
+//         <div>Sermons</div>
+//         </>
+//     )
 }
 export default Sermon;
