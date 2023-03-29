618d703102c7b8e9a4e6706cbf7dec6b2a53f737
Simion <simion.okola@student.moringaschool.com>
2023-03-29 16:18:38 +0300

    Updates

--- app/models/video.rb
+++
@@ -1,4 +0,0 @@
-class Video < ApplicationRecord
-    mount_uploader :file, VideoUploader
-    has_one_attached :clip
-end
--- app/serializers/video_serializer.rb
+++
@@ -1,3 +0,0 @@
-class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date, :file
-end
